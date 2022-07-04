f7e99bf45d4c59dbd8dbbe796e526d41ef91c0b2
SaberHardy <samirsamado5@gmail.com>
2022-07-04 19:49:37 +0300

    Initial Commit

---
+++ MyFirstIOSApp/MyFirstIOSAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFirstIOSAppApp.swift
+//  MyFirstIOSApp
+//
+//  Created by MacBook on 04.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct MyFirstIOSAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
