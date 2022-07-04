f5f38830726b98b645ffc502f9e23e66b1a6bfcd
SaberHardy <samirsamado5@gmail.com>
2022-07-04 20:18:55 +0300

    style background with colour

--- MyFirstIOSApp/ContentView.swift
+++ MyFirstIOSApp/ContentView.swift
@@ -3,11 +3,26 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var background: Color = Color.mint
+    
     var body: some View {
         ZStack {
-            Color.orange.edgesIgnoringSafeArea(.all)
+            background.edgesIgnoringSafeArea(.all)
+            VStack {
             Text("Hello, world!")
                 .padding()
+            
+            Text("Click Me!")
+                    .onTapGesture(count: 1) {
+                        background = Color.red
+                    }
+                    .padding()
+//                    .frame()
+                    .background(.mint)
+                    .cornerRadius(20)
+                
+            }
         }
     }
 }
