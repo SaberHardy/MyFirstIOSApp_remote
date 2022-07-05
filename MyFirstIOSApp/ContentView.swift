342587db43098a66620333709895e3756b3dd806
SaberHardy <samirsamado5@gmail.com>
2022-07-05 11:01:41 +0300

    design scroll view

--- MyFirstIOSApp/ContentView.swift
+++ MyFirstIOSApp/ContentView.swift
@@ -3,26 +3,15 @@
 import SwiftUI
 
 struct ContentView: View {
-    
-    @State var background: Color = Color.mint
-    
+    // 01:03:30
     var body: some View {
-        ZStack {
-            background.edgesIgnoringSafeArea(.all)
-            VStack {
-            Text("Hello, world!")
-                .padding()
-            
-            Text("Click Me!")
-                    .onTapGesture(count: 1) {
-                        background = Color.red
-                    }
-                    .padding()
-//                    .frame()
-                    .background(.mint)
-                    .cornerRadius(20)
-                
+        ScrollView(.horizontal, showsIndicators: false) {
+            HStack(spacing: 16) {
+                ForEach(0 ..< 10) { item in
+                    CardView()
+                }
             }
+            .padding()
         }
     }
 }
@@ -30,5 +19,36 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct CardView: View {
+    var body: some View {
+        VStack(alignment: .leading, spacing: 8) {
+            Image("illustration1")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(height: 200)
+                .frame(maxWidth: .infinity)
+                .cornerRadius(30)
+            
+            Text("SwiftUI for IOS 14")
+                .font(.title)
+                .fontWeight(.bold)
+                .lineLimit(3)
+            
+            Text("Complete guid to make you better in programing, and just create simple apps easily!!")
+            
+                .opacity(0.7)
+            
+        }
+        .foregroundColor(.white)
+        .padding(16)
+        .frame(width: 252, height: 329)
+        .background(LinearGradient(
+            gradient: Gradient(colors: [.white, .purple, .green]),
+            startPoint: .topLeading,
+            endPoint: .bottomTrailing))
+        .cornerRadius(30)
     }
 }
